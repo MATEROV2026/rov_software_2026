fbfa2a7cbea04b3442ae05a09922dec9a5fc7f96
Mingyue Ding <dingmingyue05@126.com>
2025-11-18 19:27:12 -0600

    USB message

--- learning_ros/learning_ros/joy_inspector_node.py
+++ learning_ros/learning_ros/joy_inspector_node.py
@@ -20,14 +20,33 @@ class JoyInspectorNode(Node):
 
         # Get the buttons vector as a simple list
         buttons_list = list(msg.buttons)
+        axes_list = list(msg.axes)
 
         # Log the formatted output
         self.get_logger().info(f"Axes:   [{axes_str}]")
         self.get_logger().info(f"Buttons: {buttons_list}\n---")
 
+        # Construct message
+        # message : <thruster_number> <pulse>
+        message = ""
 
-def init_serial():
-    ser = serial.Serial(port = "")
+        for i, button in enumerate(buttons_list):
+            if i < 8 and button == 1:
+                message += i # use a button to represent which thruster to control
+                message += " "
+                message += 1500 - axes_list[1] * 400    # readin joystick movement and change to scale (1100, 1900)
+                                                        # axes_list[1] is left stick vertical movement
+                                                        # axes : -1 up, 1 down (inverted)
+                message += "\n"
+                break
+            
+        # establish serial connection and write messages
+        ser = serial.Serial(port = "/dev/ttyACM0")
+        print(ser.name)
+        ser.write(message)
+        ser.close()
+
+
 
 def main(args=None):
     rclpy.init(args=args)
